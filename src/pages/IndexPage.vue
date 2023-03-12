99741a20bee41e53908bb400ceb2826b0626bcf0
emmanueltlom <nwabuodafiemmanuel@gmail.com>
2023-03-12 17:48:58 +0100

    homepage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,400 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="head">
+    <div class="nav">
+      <div>
+        <img class="logo" src="/images/logowhite.png" alt="" />
+      </div>
+
+      <div class="right">
+        <router-link to=""> Sign In </router-link>
+        <router-link to=""> Join Now </router-link>
+        <q-btn color="secondary"> Start Selling </q-btn>
+      </div>
+    </div>
+  </div>
+  <main class="home">
+    <div class="wrapper">
+      <div class="main_text">Find it. Buy it.</div>
+      <p>Seek and ye shall find, Buy it and it shall be delivered unto you</p>
+      <div class="input_area">
+        <form>
+          <input type="text" placeholder="Search for anything" />
+          <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+      </div>
+    </div>
+  </main>
+
+  <section class="container q-pt-md">
+    <div class="process">
+      <div class="img">
+        <img src="/images/search.png" alt="" />
+      </div>
+      <div class="img">
+        <img src="/images/contact.png" alt="" />
+      </div>
+      <div class="img">
+        <img src="/images/arrange.png" alt="" />
+      </div>
+    </div>
+  </section>
+
+  <section class="products q-pt-xl container">
+    <div class="product_cards">
+      <div v-for="(product, index) in products" :key="index" class="product">
+        <img :src="product.product_image" alt="" />
+        <div class="location">
+          <p>{{ product.location }}</p>
+        </div>
+        <div class="name">
+          <p>{{ product.name }}</p>
+        </div>
+        <div class="price">
+          <p>{{ product.amount }}</p>
+        </div>
+        <div class="desc">
+          <p>{{ product.desc }}</p>
+        </div>
+        <div class="kinds">
+          <p class="kind">{{ product.kind }}</p>
+          <p v-if="product.make !== ''" class="make">{{ product.make }}</p>
+        </div>
+        <div class="owners">
+          <p class="owner">
+            <i class="fa-solid q-mr-xs fa-gift"></i>{{ product.owner }}
+          </p>
+          <p class="ratings row q-col-gutter-x-xs items-center no-wrap">
+            <q-rating
+              v-model="ratingModel"
+              size="1.5em"
+              :max="4"
+              color="black"
+            />
+            <span>{{ product.ratings_count }}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  setup() {
+    return {
+      ratingModel: ref(4),
+    };
+  },
+  data() {
+    return {
+      products: [
+        {
+          product_image: "/images/car.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Brand New",
+          make: "Hisense",
+        },
+        {
+          product_image: "/images/sneakers.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Brand New",
+          make: "",
+        },
+        {
+          product_image: "/images/tv.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Used",
+          make: "Hisense",
+        },
+        {
+          product_image: "/images/computer.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "New",
+          make: "",
+        },
+        {
+          product_image: "/images/ac.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Brand New",
+          make: "Hisense",
+        },
+        {
+          product_image: "/images/ac.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Brand New",
+          make: "Hisense",
+        },
+        {
+          product_image: "/images/ac.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Brand New",
+          make: "",
+        },
+        {
+          product_image: "/images/ac.png",
+          name: "Royal Star 45” TV fHD/HD Smart...",
+          location: "KETU, LAGOS",
+          amount: "₦112,500",
+          desc: '32" inches hisence LED TV, tiny frame, A5 series, Design for 2022 FIFA World Cup.',
+          owner: "Izuogu & Sons Limited",
+          ratings: 5,
+          ratings_count: 203,
+          kind: "Brand New",
+          make: "Hisense",
+        },
+      ],
+    };
+  },
+});
 </script>
+
+<style scoped>
+p {
+  margin-bottom: 5px;
+}
+.head {
+  background: transparent;
+  position: absolute;
+  z-index: 10;
+  width: 100%;
+}
+.nav {
+  padding: 0.5rem;
+  align-items: center;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
+a {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  color: #ffffff;
+}
+
+.right {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.right .q-btn {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  text-transform: uppercase;
+  border-radius: 6px;
+  color: #ffffff;
+}
+
+.logo {
+  width: 157px;
+  height: 39px;
+}
+.home {
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(180deg, #1a3f55 0%, #357196 84.27%);
+}
+
+.input_area form {
+  display: flex;
+  align-items: center;
+}
+.input_area form input {
+  width: 100%;
+}
+
+.wrapper {
+  text-align: center;
+  width: 55%;
+}
+
+.wrapper .main_text {
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 58px;
+  color: #ffffff;
+}
+
+.wrapper p {
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  color: #ffffff;
+}
+
+.wrapper input {
+  padding: 0 1rem;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  color: #9a9a9a;
+  background: white;
+  border-radius: 10px 0 0 10px;
+  height: 59.63px;
+  width: 799px;
+  border: none;
+}
+
+.wrapper input::placeholder {
+  font-family: "Montserrat", sans-serif;
+}
+.wrapper button {
+  height: 59.63px;
+  background: #ee4e36;
+  border-radius: 0px 10px 10px 0px;
+  border: none;
+  width: 68.57px;
+  color: #ffffff;
+}
+.wrapper button i {
+  font-size: 1.5rem;
+}
+
+/* process */
+
+.process {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 2rem;
+  padding-top: 1rem;
+}
+
+/* prodcut card */
+.product_cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 1rem;
+  padding-top: 1rem;
+}
+
+.product_cards .product {
+  background: #ffffff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.12);
+  border-radius: 5px;
+  padding: 0.8rem;
+}
+.product_cards .product .location p {
+  font-weight: 400;
+  font-size: 8px;
+  line-height: 10px;
+  text-transform: capitalize;
+  color: #000000;
+}
+.product_cards .product .name p {
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 18px;
+  text-transform: capitalize;
+  color: #000000;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+.product_cards .product .price p {
+  font-weight: 800;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: capitalize;
+  color: #000000;
+}
+.product_cards .product .desc {
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 15px;
+  text-transform: capitalize;
+  color: #9d9d9d;
+}
+
+.product_cards .product .kinds {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin: 1rem 0 1.5rem;
+}
+.product_cards .product .kinds p {
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 12px;
+  text-transform: capitalize;
+  color: #357196;
+  background: rgba(53, 113, 150, 0.26);
+  border-radius: 5px;
+  padding: 0.5rem;
+}
+
+.product_cards .product .owners {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+.product_cards .product .owners p {
+  font-weight: 500;
+  font-size: 11px;
+  line-height: 13px;
+  text-transform: capitalize;
+  color: #000000;
+}
+.product_cards .product p.ratings {
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 12px;
+  text-transform: capitalize;
+  color: #000000;
+}
+
+@media (min-width: 1200px) {
+  .product_cards {
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
+}
+</style>
